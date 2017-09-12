c90ec43ede85da4f08d01a261bcbaba0be9e19c5
Proud Heng <pheng@ucsd.edu>
2017-09-11 19:07:14 -0700

    Adding text file with multiple lines
    Need some lines to make a merge conflict

---
+++ TrainingProgram.java
@@ -0,0 +1,8 @@
+public class TrainingProgram {
+  public static void main(String[] args) {
+    System.out.println("Robotics is fun!");
+    System.out.println("My favorite robot is R2-D2.");
+    System.out.println("Can't wait for the Velocity Vortex season!");
+    System.out.println("Better get ready!");
+  }
+}
